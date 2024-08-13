371bb01d09adcf31942551a007ea047aead7076f
MounirMkhal <mounir.mkhallati@gmail.com>
2024-08-13 14:14:37 -0700

    Set up development environment

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyCETWSL7FKcj2_4VedDTyuj2k7XiX3yJu8",
+    authDomain: "ai-flashcard-19ca2.firebaseapp.com",
+    projectId: "ai-flashcard-19ca2",
+    storageBucket: "ai-flashcard-19ca2.appspot.com",
+    messagingSenderId: "293946621798",
+    appId: "1:293946621798:web:36a10d7a240d8fb7ab52a7"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
